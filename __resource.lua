ff00e9d32041c598cb34bc9a3d39a1e8f70e2af2
MilutuS <33899365+MilutuS@users.noreply.github.com>
2019-11-09 08:24:51 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+-- Jeśli potrzebujesz pomocy dołącz na https://discord.gg/GgaMnNC
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+version '1.0.0'
+description 'esx_makeweapons by Znajak'
+client_scripts{
+	'config.lua',
+	'client.lua'
+}
+server_scripts{
+	'config.lua',
+	'server.lua'
+}
+-- Jeśli potrzebujesz pomocy dołącz na https://discord.gg/GgaMnNC
